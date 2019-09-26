252ea3046711aeb91ed8ccc42ccd2f3a7120cc91
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2019-09-27 06:22:28 +0800

    Initialised

---
+++ src/renderer/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'bickert-tracker-app',
+    mounted() {
+
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+	
+export default new Router({
+	routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: require('@/views/Login').default
+        },
+        {
+            path: '/',
+            name: 'landing-page',
+            component: require('@/components/LandingPage').default
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+	]
+})
