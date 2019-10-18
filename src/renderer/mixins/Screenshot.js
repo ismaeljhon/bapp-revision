cca5c1cf7eb6236ba3a9ada3eea90ab0517771d8
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2019-10-18 17:51:50 +0800

    Update screenshot naming convention

--- src/renderer/mixins/Screenshot.js
+++ src/renderer/mixins/Screenshot.js
@@ -2,12 +2,19 @@ import { takeScreenshot, Data } from "screenshot-monitor-capture";
 const fs = require('fs');
 import { rootPath } from 'electron-root-path';
 import moment from 'moment';
+import _find from 'lodash/find';
+import _replace from 'lodash/replace';
 
 let Screenshot = {
     methods: {
         captureScreenshot() {
+            let users = JSON.parse(localStorage.ZOHO_USERS);
+            let currentUser =  _find(users, o => {
+                return o.email == localStorage.ZOHO_EMAIL;
+            });
+
             takeScreenshot("image/png").then((data) => {
-                fs.writeFile(rootPath + "/screenshots/-" + moment().format('DDMMYYYY-hhmmss') + ".jpg", data.buffer, (error) => {
+                fs.writeFile(rootPath + "/screenshots/"+ _replace(currentUser.name, ' ', '') +"-" + moment().format('DDMMYYYY-hhmmss') + ".jpg", data.buffer, (error) => {
                     if (error) {
                         console.error(error);
                     }
