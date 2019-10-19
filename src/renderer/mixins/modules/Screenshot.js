3c10b1823643aa512b86e1e201edb00a761168b3
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2019-10-19 13:59:57 +0800

    Remove unused variable ata

--- src/renderer/mixins/modules/Screenshot.js
+++ src/renderer/mixins/modules/Screenshot.js
@@ -1,4 +1,4 @@
-import { takeScreenshot, Data } from "screenshot-monitor-capture";
+import { takeScreenshot } from "screenshot-monitor-capture";
 const fs = require('fs');
 import { rootPath } from 'electron-root-path';
 import moment from 'moment';
