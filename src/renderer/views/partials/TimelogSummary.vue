7ba12ed834bd6b8f9ab1a2346364e36649dd1678
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2019-11-27 20:18:32 +0800

    Add info on the timelog summary

--- src/renderer/views/partials/TimelogSummary.vue
+++ src/renderer/views/partials/TimelogSummary.vue
@@ -3,9 +3,12 @@
         <strong>Today: </strong><span>{{ dailyTimeLogs }}</span>
         <br>
         <strong>This Week: </strong><span>{{ weeklyTimelogs }}</span>
+        <br>
+        <small class="mt-2 d-block">Zoho Projects Time Zone: <br>Edmonton, AB, Canada (GMT-7)</small>
     </b-card>
 </template>
 <script>
+import moment from 'moment-timezone';
 export default {
     name: 'timelog-summary',
     computed: {
@@ -17,6 +20,9 @@ export default {
             let dailyTimeLogs = this.$store.getters.DAILY_TIMELOGS || [];
             return dailyTimeLogs.grandtotal ? this.customHourFormat(dailyTimeLogs.grandtotal) : '0h 0m';
         }
+    },
+    mounted() {
+        console.log(moment.tz.guess())
     }
 }
 </script>
