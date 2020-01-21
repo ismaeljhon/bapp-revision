d1963ab4b14df35dac7cc11cfbf8b74bcf51e30a
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-01-21 20:58:32 +0800

    Add downloadable timesheet

--- src/renderer/views/modals/Timesheet.vue
+++ src/renderer/views/modals/Timesheet.vue
@@ -7,21 +7,27 @@
             <date-picker format="MM-dd-yyyy" v-model="form.date" input-class="form-control"></date-picker>
         </b-form-group>
         <template slot="modal-footer">
-            <b-button variant="primary" @click.prevent="processTimesheet">
-                <font-awesome-icon icon="download"></font-awesome-icon> Generate and Download
+            <b-button v-if="!isTimesheetReady" variant="primary" @click.prevent="processTimesheet" :disabled="isLoading">
+                <font-awesome-icon icon="sync"></font-awesome-icon> Generate Timesheet
             </b-button>
+            <download-csv v-else class="btn btn-success" :data="json_data" name="timesheet">
+                <font-awesome-icon icon="check"></font-awesome-icon> Your timesheet is ready... Click here to download.
+            </download-csv>
         </template>
     </b-modal>
 </template>
 <script>
 import _assign from 'lodash/assign';
+import _forEach from 'lodash/forEach';
 import moment from 'moment-timezone';
 import Datepicker from 'vuejs-datepicker';
+import JsonCSV from 'vue-json-csv'
 
 export default {
     name: 'timesheet-modal',
     components: {
-        'date-picker': Datepicker
+        'date-picker': Datepicker,
+        downloadCsv: JsonCSV
     },
     data() {
         return {
@@ -31,10 +37,13 @@ export default {
                 view_type: 'week',
             },
             timesheet: [],
+            json_data: [],
             viewTypeOptions: [
                 { value: 'week', text: 'Weekly' },
                 { value: 'day', text: 'Daily' }
-            ]
+            ],
+            isLoading: false,
+            isTimesheetReady: false,
         }
     },
     methods: {
@@ -47,16 +56,39 @@ export default {
                 form: {
                     date: moment().format('MM-DD-YYYY'),
                     view_type: 'week',
-                }
+                },
             });
         },
         async processTimesheet() {
             _assign(this.form, {
                 date: moment(this.form.date).format('MM-DD-YYYY'),
-                doNotSaveToLocalStorage: false
+                doNotSaveToLocalStorage: true
             })
+            
+            this.isLoading = true;
+            this.timesheet = await this.fetchWeeklyTimelogs(this.form);
+            _forEach(this.timesheet.date, timelog => {
+                let logObj = {
+                    date: timelog.date
+                };
+                _forEach(timelog.tasklogs, log => {
+                    _assign(logObj, {
+                        'project_name': log.project ? log.project.name : '',
+                        'task': log.task.name,
+                        'hours': log.hours_display,
+                        'cost': log.cost,
+                        'from_to': (log.start_time && log.end_time) ? log.start_time + " - " + log.end_time : '-',
+                        'task_list': log.task.name,
+                        'owner': log.owner_name,
+                        'billing_status': log.bill_status,
+                        'approval_status': log.approval_status
+                    })
 
-            this.timesheet = await this.fetchWeeklyTimelogs(this.form)
+                    this.json_data.push(logObj);
+                })
+            })
+            this.isLoading = false;
+            this.isTimesheetReady = true;
         }
     }
 }
