b8c72b6b1e9d72f47f39bf79b638823f3e7ed856
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-07-09 21:09:38 +0800

    Add mac support

--- src/renderer/config/bickert-timetracker.oauth.js
+++ src/renderer/config/bickert-timetracker.oauth.js
@@ -1,7 +1,9 @@
 import { rootPath } from 'electron-root-path'
 
+const darwinContent = (process.platform == 'darwin' && process.env.NODE_ENV != 'development') ? "/Contents" : ""
+
 export default {
-    config_file_path: rootPath + "/oauthkeys/bickert-timetracker.config.json",
+    config_file_path: rootPath + darwinContent + "/oauthkeys/bickert-timetracker.config.json",
     base_url: "https://accounts.zoho.com",
     url: {
         refresh_token: "/oauth/v2/token"
