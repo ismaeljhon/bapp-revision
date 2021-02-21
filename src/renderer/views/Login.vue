8ed616bb2979c71ecd1faaa5d954f51fd7473a97
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2021-02-21 15:35:18 +0800

    Fix Indentions

--- src/renderer/views/Login.vue
+++ src/renderer/views/Login.vue
@@ -1,55 +1,67 @@
 <template>
-    <b-card header="Login" header-tag="header" tag="article" class="mt-5">
-        <b-form @submit.prevent="onSubmit">
-            <b-form-group label="Email address:" description="Use your zoho email here" :invalid-feedback="veeErrors.first('email')" :state="!veeErrors.has('email')">
-                <b-form-input v-model="form.email" name="email" v-validate="{ required: true, email: true }"></b-form-input>
-            </b-form-group>
-            <b-button type="submit" variant="primary" :disabled="disableBtn">Start Working</b-button>
-        </b-form>
-    </b-card>
+  <b-card header="Login" header-tag="header" tag="article" class="mt-5">
+    <b-form @submit.prevent="onSubmit">
+      <b-form-group
+        label="Email address:"
+        description="Use your zoho email here"
+        :invalid-feedback="veeErrors.first('email')"
+        :state="!veeErrors.has('email')"
+      >
+        <b-form-input
+          v-model="form.email"
+          name="email"
+          v-validate="{ required: true, email: true }"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary" :disabled="disableBtn"
+        >Start Working</b-button
+      >
+    </b-form>
+  </b-card>
 </template>
 
 <script>
-    import _find from 'lodash/find';
-    import Log from '@/shared/Log'
+import _find from "lodash/find";
+import Log from "@/shared/Log";
 
-    export default {
-        name: 'login',
-        data() {
-            return {
-                form: {},
-                users: [],
-                disableBtn: false
-            }
-        },
-        methods: {
-            onSubmit(){
-                this.$validator.validateAll().then(noerrors => {
-                    if (noerrors) {
-                        let users = this.getUsers();
+export default {
+  name: "login",
+  data() {
+    return {
+      form: {},
+      users: [],
+      disableBtn: false,
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$validator.validateAll().then((noerrors) => {
+        if (noerrors) {
+          let users = this.getUsers();
 
-                        let currentUser = _find(users, o => { return o.email == this.form.email });
+          let currentUser = _find(users, (o) => {
+            return o.email == this.form.email;
+          });
 
-                        if (currentUser) {
-                            localStorage.ZOHO_CURRENT_USER = JSON.stringify(currentUser);
-                            this.$router.push('/');
-                        } else {
-                            Log.warning('Email not Registered', { withPrompt: true })
-                        }
-                    }
-
-                    return false;
-                })
-            },
-        },
-        mounted: async function() {
-            this.disableBtn = true;
-            await this.fetchUsers();
-            this.users = localStorage.ZOHO_USERS;
-            this.disableBtn = false;
+          if (currentUser) {
+            localStorage.ZOHO_CURRENT_USER = JSON.stringify(currentUser);
+            this.$router.push("/");
+          } else {
+            Log.warning("Email not Registered", { withPrompt: true });
+          }
         }
-    }
+
+        return false;
+      });
+    },
+  },
+  mounted: async function() {
+    this.disableBtn = true;
+    await this.fetchUsers();
+    this.users = localStorage.ZOHO_USERS;
+    this.disableBtn = false;
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
