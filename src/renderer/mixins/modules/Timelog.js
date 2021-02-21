6fca061c78a194f49f10c873f17fc6945d8bcb20
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2021-02-21 18:46:34 +0800

    Fix: issue on fetching weekly and daily timelog - cannnot find current user as we updated it to use the zoho people instead of zoho projects

--- src/renderer/mixins/modules/Timelog.js
+++ src/renderer/mixins/modules/Timelog.js
@@ -1,151 +1,207 @@
-import RestApiService from '@/services/RestApiService';
-import moment from 'moment-timezone';
-import _join from 'lodash/join';
-import _assign from 'lodash/assign';
-import _find from 'lodash/find';
-import swal from 'sweetalert';
-import Log from '@/shared/Log';
+import RestApiService from "@/services/RestApiService";
+import moment from "moment-timezone";
+import { join as _join, assign as _assign, find as _find } from "lodash";
+import swal from "sweetalert";
+import Log from "@/shared/Log";
+import { getCurrentZohoProjectUser } from "@/mixins/modules/User";
 
 let Timelog = {
-    methods: {
-        recordTimelog(rawData) {
-            _assign(rawData, this.getTimelogData(rawData));
-            localStorage.setItem('ZOHO_LAST_TIME_LOG', JSON.stringify(rawData));
-            Log.info("Record Timelog: " + JSON.stringify(rawData), { processType: 'process' });
-            return true;
-        },
-        pushTimelog(rawData) {
-            let timelogData = this.getTimelogData(rawData);
+  methods: {
+    recordTimelog(rawData) {
+      _assign(rawData, this.getTimelogData(rawData));
+      localStorage.setItem("ZOHO_LAST_TIME_LOG", JSON.stringify(rawData));
+      Log.info("Record Timelog: " + JSON.stringify(rawData), {
+        processType: "process",
+      });
+      return true;
+    },
+    pushTimelog(rawData) {
+      let timelogData = this.getTimelogData(rawData);
 
-            let suffix = " - " + moment().format("MMDDYYYHHmmss");
-            let notes = "timesheet log : start time -" + localStorage.ZOHO_TIME_STARTED + " end time -" + moment().format('MM-DD-YYYY HH:mm A');
-            notes += timelogData.notes ? timelogData.notes + suffix : suffix;
-            timelogData.notes = notes;
+      let suffix = " - " + moment().format("MMDDYYYHHmmss");
+      let notes =
+        "timesheet log : start time -" +
+        localStorage.ZOHO_TIME_STARTED +
+        " end time -" +
+        moment().format("MM-DD-YYYY HH:mm A");
+      notes += timelogData.notes ? timelogData.notes + suffix : suffix;
+      timelogData.notes = notes;
 
-            let taskId = rawData.subTask ? rawData.subTask.id : rawData.task_id;
-            
-            Log.info("Pushing Timelog " + JSON.stringify(rawData), { processType: 'request' });
+      let taskId = rawData.subTask ? rawData.subTask.id : rawData.task_id;
 
-            return new RestApiService('/portal/' + process.env.VUE_APP_PORTAL_ID + "/projects/" + rawData.project_id + "/tasks/" + taskId + "/logs/")
-                .save({ params: timelogData }, true)
-                    .then(response => {
-                        localStorage.ZOHO_LAST_TIME_LOG = '';
-                        Log.success("Timelog has been successfully pushed", { rawData: rawData, timer: 1500, withPrompt: true, processType: 'response' })
-                    }).catch(error => {
-                        Log.error(error, { processType: 'response', customMessage: "Error on pushing your Timelog" });
-                    });
-        },
-        getTimelogData(data) {
-            if (!data)
-                return;
-                
-            let currentUser = this.getCurrentUser();
-            let timeConsumed = data.timeConsumed.split(':');
-            timeConsumed.pop();
-            timeConsumed = timeConsumed.join(":");
+      Log.info("Pushing Timelog " + JSON.stringify(rawData), {
+        processType: "request",
+      });
 
-            return {
-                date: moment().format('MM-DD-YYYY'),
-                owner: currentUser.id,
-                hours: timeConsumed,
-                bill_status: 'Billable',
-                notes: data.notes
-            };
-        },
-        getPendingTimelog() {
-            let pendingTimelog = localStorage.ZOHO_LAST_TIME_LOG
-            if (!pendingTimelog) return false
-            return JSON.parse(pendingTimelog)
+      return new RestApiService(
+        "/portal/" +
+          process.env.VUE_APP_PORTAL_ID +
+          "/projects/" +
+          rawData.project_id +
+          "/tasks/" +
+          taskId +
+          "/logs/"
+      )
+        .save({ params: timelogData }, true)
+        .then((response) => {
+          localStorage.ZOHO_LAST_TIME_LOG = "";
+          Log.success("Timelog has been successfully pushed", {
+            rawData: rawData,
+            timer: 1500,
+            withPrompt: true,
+            processType: "response",
+          });
+        })
+        .catch((error) => {
+          Log.error(error, {
+            processType: "response",
+            customMessage: "Error on pushing your Timelog",
+          });
+        });
+    },
+    getTimelogData(data) {
+      if (!data) return;
 
-            let currentUser = this.getCurrentUser();
+      let currentUser = this.getCurrentUser();
+      let timeConsumed = data.timeConsumed.split(":");
+      timeConsumed.pop();
+      timeConsumed = timeConsumed.join(":");
 
-            if (currentUser.id == pendingTimelogs.owner) {
-                swal({
-                    title: "Unsaved Timelog",
-                    closeOnClickOutside: false,
-                    closeOnEsc: false,
-                    text: pendingTimelogs.project.name + "\n\n" + pendingTimelogs.task.name + "\n\n" + "Time Spent: " + pendingTimelogs.hours,
-                    icon: "warning",
-                    buttons: {
-                        do_not_push: {
-                            text: 'Do not Push',
-                            value: false
-                        },
-                        push: {
-                            text: 'Push Timelog',
-                            value: true
-                        }
-                    },
-                }).then(async (pushTimelog) => {
-                    if (pushTimelog) {
-                        this.$store.commit('SET_TIMER_READY')
-                        await this.pushTimelog(pendingTimelogs);
-                        this.$store.commit('SET_TIMER_READY')
-                    } else {
-                        Log.warning("Discard unsaved timelogs", { processType: 'process' })
-                        localStorage.ZOHO_LAST_TIME_LOG = '';
-                    }
-                })
-            }
-        },
-        fetchWeeklyTimelogs(overrideOptions = {}) {
-            let params = {
-                users_list: this.getCurrentUser().id,
-                view_type: overrideOptions.view_type ? overrideOptions.view_type : 'week',
-                date: overrideOptions.date ? overrideOptions.date : moment().format('MM-DD-YYYY'),
-                bill_status: 'All',
-                component_type: 'task'
-            };
+      return {
+        date: moment().format("MM-DD-YYYY"),
+        owner: currentUser.id,
+        hours: timeConsumed,
+        bill_status: "Billable",
+        notes: data.notes,
+      };
+    },
+    getPendingTimelog() {
+      let pendingTimelog = localStorage.ZOHO_LAST_TIME_LOG;
+      if (!pendingTimelog) return false;
+      return JSON.parse(pendingTimelog);
 
-            Log.info("Fetch weekly Timelog ", { processType: 'request' })
+      let currentUser = this.getCurrentUser();
 
-            return new RestApiService('/portal/' + process.env.VUE_APP_PORTAL_ID + '/logs')
-            .index(params)
-                .then(response => {
-                    let timelogs = response.data.timelogs || {};
+      if (currentUser.id == pendingTimelogs.owner) {
+        swal({
+          title: "Unsaved Timelog",
+          closeOnClickOutside: false,
+          closeOnEsc: false,
+          text:
+            pendingTimelogs.project.name +
+            "\n\n" +
+            pendingTimelogs.task.name +
+            "\n\n" +
+            "Time Spent: " +
+            pendingTimelogs.hours,
+          icon: "warning",
+          buttons: {
+            do_not_push: {
+              text: "Do not Push",
+              value: false,
+            },
+            push: {
+              text: "Push Timelog",
+              value: true,
+            },
+          },
+        }).then(async (pushTimelog) => {
+          if (pushTimelog) {
+            this.$store.commit("SET_TIMER_READY");
+            await this.pushTimelog(pendingTimelogs);
+            this.$store.commit("SET_TIMER_READY");
+          } else {
+            Log.warning("Discard unsaved timelogs", { processType: "process" });
+            localStorage.ZOHO_LAST_TIME_LOG = "";
+          }
+        });
+      }
+    },
+    fetchWeeklyTimelogs(overrideOptions = {}) {
+      let params = {
+        users_list: getCurrentZohoProjectUser().id,
+        view_type: overrideOptions.view_type
+          ? overrideOptions.view_type
+          : "week",
+        date: overrideOptions.date
+          ? overrideOptions.date
+          : moment().format("MM-DD-YYYY"),
+        bill_status: "All",
+        component_type: "task",
+      };
 
-                    if (overrideOptions.doNotSaveToLocalStorage) {
-                        return timelogs;
-                    }
+      Log.info("Fetch weekly Timelog ", { processType: "request" });
 
-                    localStorage.setItem('ZOHO_WEEKLY_TIMELOGS', JSON.stringify(timelogs));
-                    this.$store.commit('SET_WEEKLY_TIMELOGS', timelogs)
+      return new RestApiService(
+        "/portal/" + process.env.VUE_APP_PORTAL_ID + "/logs"
+      )
+        .index(params)
+        .then((response) => {
+          let timelogs = response.data.timelogs || {};
 
-                    Log.info("Weekly Timelog successfully fetch " + JSON.stringify(timelogs), { processType: 'response' })
-                }).catch(error => {
-                    Log.error(error, { processType: 'response', customMessage: "Error on fetching weekly timelog" });
-                });;
-        },
-        getWeeklyTimelogs() {
-            return JSON.parse(localStorage.ZOHO_WEEKLY_TIMELOGS);
-        },
-        fetchDailyTimelogs() {
-            let params = {
-                users_list: this.getCurrentUser().id,
-                view_type: 'day',
-                date: moment().tz('America/Edmonton').format('MM-DD-YYYY'),
-                bill_status: 'All',
-                component_type: 'task'
-            };
+          if (overrideOptions.doNotSaveToLocalStorage) {
+            return timelogs;
+          }
 
-            Log.info("Fetch daily Timelog ", { processType: 'request' })
+          localStorage.setItem(
+            "ZOHO_WEEKLY_TIMELOGS",
+            JSON.stringify(timelogs)
+          );
+          this.$store.commit("SET_WEEKLY_TIMELOGS", timelogs);
 
-            return new RestApiService('/portal/' + process.env.VUE_APP_PORTAL_ID + '/logs')
-            .index(params)
-                .then(response => {
-                    let timelogs = response.data.timelogs || {};
-                    localStorage.setItem('ZOHO_DAILY_TIMELOGS', JSON.stringify(timelogs));
-                    this.$store.commit('SET_DAILY_TIMELOGS', timelogs)
+          Log.info(
+            "Weekly Timelog successfully fetch " + JSON.stringify(timelogs),
+            { processType: "response" }
+          );
+        })
+        .catch((error) => {
+          Log.error(error, {
+            processType: "response",
+            customMessage: "Error on fetching weekly timelog",
+          });
+        });
+    },
+    getWeeklyTimelogs() {
+      return JSON.parse(localStorage.ZOHO_WEEKLY_TIMELOGS);
+    },
+    fetchDailyTimelogs() {
+      let params = {
+        users_list: getCurrentZohoProjectUser().id,
+        view_type: "day",
+        date: moment()
+          .tz("America/Edmonton")
+          .format("MM-DD-YYYY"),
+        bill_status: "All",
+        component_type: "task",
+      };
 
-                    Log.info("Daily Timelog successfully fetch " + JSON.stringify(timelogs), { processType: 'response' })
-                }).catch(error => {
-                    Log.error(error, { processType: 'response', customMessage: "Error on fetching weekly timelog" });
-                });
-        },
-        getDailyTimelogs() {
-            return JSON.parse(localStorage.ZOHO_DAILY_TIMELOGS);
-        },
-    }
-}
+      Log.info("Fetch daily Timelog ", { processType: "request" });
+
+      return new RestApiService(
+        "/portal/" + process.env.VUE_APP_PORTAL_ID + "/logs"
+      )
+        .index(params)
+        .then((response) => {
+          let timelogs = response.data.timelogs || {};
+          localStorage.setItem("ZOHO_DAILY_TIMELOGS", JSON.stringify(timelogs));
+          this.$store.commit("SET_DAILY_TIMELOGS", timelogs);
+
+          Log.info(
+            "Daily Timelog successfully fetch " + JSON.stringify(timelogs),
+            { processType: "response" }
+          );
+        })
+        .catch((error) => {
+          Log.error(error, {
+            processType: "response",
+            customMessage: "Error on fetching weekly timelog",
+          });
+        });
+    },
+    getDailyTimelogs() {
+      return JSON.parse(localStorage.ZOHO_DAILY_TIMELOGS);
+    },
+  },
+};
 
 export default Timelog;
